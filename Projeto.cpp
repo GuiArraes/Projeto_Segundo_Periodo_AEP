43ec3769005b80c3d56c0e3dcdfb3b8235d9305e
Guilherme Arraes <guilhermeaca@gmail.com>
2022-01-05 20:31:19 -0300

    Versão 3.0 - 26/08/2021

--- Projeto.cpp
+++ Projeto.cpp
@@ -3,178 +3,202 @@
 #include <locale.h>
 #include <string.h> // função strlen(char) - conta caracteres
                     //função strcpy(char) - copia caracter
-
-
 struct endereco {
-       char logra[20];
-       int CEP;
-       char num[7];
-       char complemento[20];
-       char bairro[20];
-       char cidade[20];     
+       char logra[20], num[7], complemento[20], bairro[20], cidade[20];
+       int CEP;    
 };
-
 struct telefone {
        int celular[11];       
 };
-
 struct cadastro{
-       char nome[20];
-       char email[20];
-       int CPF[15];
+       char nome[20], email[20], CPF[15], senha[10], senhaR[10];
        //int senha;
        //int senhaR;
-       char senha[10];
-       char senhaR[10];
        struct endereco Endereco;
        struct telefone Telefone;       
 };
 //------------------------------------------------------------------------------
-void cadastro();
-void busca ();
-void lendoCadastro ();
+struct cadastroMaq {
+       char nomeMaq[20], catMaq[20], marMaq[20], modMaq[20], anoMaq[7], potMaq[10];
+       //int anoMaq;
+       //int potMaq; 
+};
+//------------------------------------------------------------------------------
+//Variaveis Golobais
+struct cadastro cad;
+//------------------------------------------------------------------------------
+
+void cadCliente();
+void enterCadCliente (int c);
+
+void cadMaquina (int M);
+
+void diagramacao (int n);
+void topo ();
+//------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 int main (){
     setlocale(LC_ALL,"portuguese");
     
-    //cadastro ();
-    //busca ();
-    lendoCadastro ();
     
-    
-    //system ("cls");
-    printf ("---------------------");
-    for (int i=0; i<=30; i++){
-        printf ("%s", textoNome[i]);
-    }
+    system("mode con:cols=120 lines=40"); //Tamenho da tela
     
 
-system ("pause");
+
+    cadCliente ();
+    //cadMaquina (1);
+
+    system ("pause");
 return 0;
 }
 //------------------------------------------------------------------------------
-void cadastro (){
-     //int x;
-     FILE *cadastroCliente;
-     cadastroCliente = fopen ("CadastroCliente.txt","a");
-     struct cadastro cad;
-     
-     if (cadastroCliente == NULL){
-        printf ("Erro: O Arquivo 'Cadastro Cliente' não foi aberto!\n");
+//------------------------------------------------------------------------------
+void diagramacao (int n){
+     for (int x= 0; x<=n; x++){
+         printf ("\n");
      }
-
-     
-     printf ("Nome: ");
-     gets (cad.nome);
-
-
-  // do{
-         fflush(stdin);
-         printf ("Senha: ");
-         //scanf ("%d",&cad.senha);
-         gets (cad.senha);
-
-         printf ("Repetir Senha: ");
-         //scanf ("%d",&cad.senhaR);
-         gets (cad.senhaR);     
-   //}while ((cad.senha) != (cad.senhaR)); //As duas senhas tem que serem iguais.
-   
-   int k;
-   k = fputs (cad.nome, cadastroCliente);
-   k = fputs (cad.senha, cadastroCliente); //Armazena STRING no Formato: txt.
-   k = fputs ("\n", cadastroCliente);
-   if (k == EOF){
-      printf ("Erro ao gravar os Dados no arquivo 'CadastroCliente'!");
-   }
-
-   //fwrite (&cad,sizeof(struct cadastro),1,cadastroCliente);   //Armazena um Bloco de Dados Formato: Binario
-   fclose (cadastroCliente);
-
 }
 //------------------------------------------------------------------------------
-void busca (){
-     FILE *cadastroCliente;
-     cadastroCliente = fopen ("CadastroCliente.txt","r");
+void topo (){
+     system ("cls");
+     system ("color F0"); //Muda a cor do fundo e letra ("color(fundo)(letra)") Hexadecimal*** 
      
-     if (cadastroCliente == NULL){
-        printf ("Erro: O Arquivo 'Cadastro Cliente' não foi aberto!\n");
-     }
+     printf ("RENTagro\t\t\t\t\t\t\t\t\t\t    AJUDA  CADASTRA-SE  ENTRAR\n");
      
-     char leitura[30];
-     
-     //fgets(leitura,)
-     
-     fclose (cadastroCliente);
      /*
-     struct cadastro cad; 
-     printf ("-----------\n");
-     while (fread (&cad,sizeof(struct cadastro),1,cadastroCliente)==1){
-           printf ("%d \n",cad.senha);
-     }
-     printf ("-----------\n"); 
-     */    
+     printf(" _____  ______ _   _ _______                    \n");                    
+     printf("|  __ \|  ____| \ | |__   __|                   \n");                   
+     printf("| |__) | |__  |  \| |  | | __ _  __ _ _ __ ___  \n");  
+     printf("|  _  /|  __| | . ` |  | |/ _` |/ _` | '__/ _ \ \n");
+     printf("| | \ \| |____| |\  |  | | (_| | (_| | | | (_) |\n");
+     printf("|_|  \_\______|_| \_|  |_|\__,_|\__, |_|  \___/ \n");
+     printf("                                 __/ |          \n");          
+     printf("                                |___/           \n");
+     */
+
 }
 //------------------------------------------------------------------------------
-
-/* Essa ideia não da certo!!!
-void lendoCadastro (){
-     FILE *cadastroCliente;
-   
-    char textoNome[30]; //Amarzenar os nomes que são do arquivo CadastroCliente.
-    char texto [30];
+void cadCliente (){
+     topo();
+     diagramacao (10);
      
-     cadastroCliente = fopen ("CadastroCliente.txt","r");
-     for (int i=0; i<=30; i++){
-         fgets(texto,30,cadastroCliente);
-         textoNome [i] = texto;
-     }
-     fclose (cadastroCliente);
+     /*
+      memset (&cad.nome, 0, sizeof (cad.nome) ); //Limpa a memória de mod.nome ...
+      memset (&cad.email, 0, sizeof (cad.email) );
+      memset (&cad.CPF, 0, sizeof (cad.CPF) );
+      memset (&cad.senha, 0, sizeof (cad.senha) );
+      memset (&cad.senhaR, 0, sizeof (cad.senhaR) );
+     */
+     //for (int i=0; i<=N; i++){
+         fflush (stdin);
+         printf ("\t\t\t\t\t________________________________________\n");
+         printf ("\t\t\t\t\tNome          :%s \n",cad.nome);
+         //gets (cad[i].nome);
+         printf ("\t\t\t\t\t========================================\n");
+         
+         fflush (stdin);
+         printf ("\t\t\t\t\tE-mail        :%s \n",cad.email);
+         //gets (cad[i].email);
+         //Validar e-mail
+         printf ("\t\t\t\t\t========================================\n");
+         
+         fflush (stdin);
+         printf ("\t\t\t\t\tCPF           :%s \n",cad.CPF);
+         //gets (cad[i].CPF);
+         printf ("\t\t\t\t\t========================================\n");
+     //Função para validar o CPF
+         // do{
+             fflush(stdin);
+             printf ("\t\t\t\t\tSenha         :%s \n",cad.senha);
+             //scanf ("%d",&cad.senha);
+             //gets (cad[i].senha);
+             printf ("\t\t\t\t\t========================================\n");
+             
+             fflush (stdin);
+             printf ("\t\t\t\t\tRepetir Senha :%s \n",cad.senhaR);
+             //scanf ("%d",&cad.senhaR);
+             //gets (cad[i].senhaR);
+             printf ("\t\t\t\t\t________________________________________\n");     
+         //}while ((cad.senha) != (cad.senhaR)); //As duas senhas tem que serem iguais.
+
+            for (int c=1; c<=5; c++){ 
+                enterCadCliente (2);
+            }
+ 
+     //}    
 }
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         //p -> senhaR = x;
-//printf ("\n\n%d \n\n",p -> senhaR);   //TESTESSSS
-
-
-
-
+//------------------------------------------------------------------------------
+void enterCadCliente (int c){         
+         switch (c){
+                case 1: 
+                     printf ("\n\t\t\t\t\tNome:");
+                     gets (cad.nome);
+                     break; 
+                case 2:
+                     int contEmail = 0;
+                     char validaEmail; //Pegar um caracter por vez
+                     do {
+                        system ("cls");
+                        cadCliente (); 
+                        printf ("\n\t\t\t\t\tE-mail:");
+                        gets (cad.email);
+                     }while (contEmail >= 2);
+                     break;
+                case 3:
+                     printf ("\n\t\t\t\t\tCPF:");
+                     gets (cad.nome);
+                     break;
+                case 4:
+                     printf ("\n\t\t\t\t\tSenha:");
+                     gets (cad.senha);
+                     break;
+                case 5:
+                     printf ("\n\t\t\t\t\tRepetir Senha:");
+                     gets (cad.senhaR);
+                     break;
+         }     
+}
+//------------------------------------------------------------------------------
+void cadMaquina (int M){
+     printf ("%d \n",M);
+     struct cadastroMaq cadMaq[M];
+     for (int i=0; i<=M; i++){
+         printf ("%d",i);
+         fflush (stdin);
+         printf ("Nome do Maquinário: \n");
+         //gets (cadMaq[i].nomeMaq);
+         
+         fflush (stdin);
+         printf ("Categoria do Maquinário: \n");
+         //gets (cadMaq[i].catMaq);
+         
+         fflush (stdin);
+         printf ("Marca do Maquinário: \n");
+         //gets (cadMaq[i].marMaq);
+         //Tem um erro aqui********************************************
+         fflush (stdin);
+         printf ("Modelo do Maquinário: \n");
+         //gets (cadMaq[i].modMaq);
+         
+         fflush (stdin);
+         printf ("Ano do Maquinário: \n");
+         //gets (cadMaq[i].anoMaq); 
+         
+         fflush (stdin);
+         printf ("Potência do Maquinário: \n");
+         //gets(cadMaq[i].potMaq);     
+         /*
+         fflush (stdin);
+         printf ("Ano do Maquinário: ");
+         scanf ("%d",cadMaq[i].anoMaq);
+         fflush (stdin);
+         printf ("Potência do Maquinário: ");
+         scanf ("%d",cadMaq[i].potMaq);
+         */
+     }
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 //Cadastro
 /* TEM ERRO AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII Duvida professor!!!!!
@@ -194,8 +218,4 @@ void lendoCadastro (){
       } 
    }while (validoE >= 2);
      
-   printf ("CPF: ");
-   scanf ("%1d",&cad[i].CPF);
-   //fflush (stdin);
-   validaCPF(cad[i].CPF);
 */
